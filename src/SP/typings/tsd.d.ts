062a85615b125227b56fa9b5b1481b5e2a4530cc
oshalygin@gmail.com <oshalygin@gmail.com>
2015-09-27 23:51:57 -0700

    tsd angular files

--- src/SP/typings/tsd.d.ts
+++ src/SP/typings/tsd.d.ts
@@ -1,3 +1,7 @@
 
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
+/// <reference path="angularjs/angular-resource.d.ts" />
+/// <reference path="angularjs/angular-animate.d.ts" />
+/// <reference path="angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="angular-ui-bootstrap/angular-ui-bootstrap.d.ts" />
