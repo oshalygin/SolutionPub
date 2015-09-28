f05bc5132d2efee136aa4878e58ce6769067acb1
oshalygin@gmail.com <oshalygin@gmail.com>
2015-09-27 23:29:26 -0700

    cleaning up files

---
+++ src/SP/application/app.ts
@@ -0,0 +1,14 @@
+((): void => {
+    "use strict";
+
+    angular.module("app", [
+        "app.core",
+        "app.services",
+        "app.layout",
+        "app.home",
+        "app.post",
+        "app.tag"
+
+    ]);
+
+})();
