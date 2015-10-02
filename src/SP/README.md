3bd907d88bf65d3601dec12592b3b5378d4ed411
oshalygin@gmail.com <oshalygin@gmail.com>
2015-10-01 21:19:40 -0700

    added a readme.md file, and started to bring in node packages

---
+++ src/SP/README.md
@@ -0,0 +1,8 @@
+##Solution Pub
+###Built using ASP.NET 5 and AngularJS
+
+Developed by Oleg Shalygin, primarily to make the web a better place.
+
+If you have any questions, feel free to drop me a line at oshalygin@gmail.com!
+
+Visit the current blog at www.techiejs.com
---
+++ src/SP/gulpconfig.js
@@ -0,0 +1,71 @@
+module.exports = function () {
+
+    var deploymentPath = "./app_deploy/";
+    var devPath = "./application";
+    var appJsDev = devPath + "**/*.js";
+    var bowerJson = require("./bower.json");
+    var layout = "./Views/Shared/";
+    var layoutInjector = "./_Layout.cshtml";
+
+    var config = {
+        appJavaScriptFiles:
+        [
+            //Gulp Files
+            "./gulp.config.js", "./gulpfile.js",
+            //Angular Files
+            appJsDev
+        ],
+
+        tsTypingDefinitions: "./typings/**/*.d.ts",
+
+        appTypeScriptFiles: [
+            devPath + "**/*.ts"
+        ],
+
+        appTsDev: devPath + "**/*.ts",
+        appJsDev: appJsDev,
+        appJsDepolyment: deploymentPath + "**/*.js",
+
+        appDeployFolder: deploymentPath,
+        appDevFolder: devPath,
+
+        bower: {
+            json: bowerJson,
+            directory: "./bower_components/",
+            ignorePath: "../.."
+        },
+
+
+        js: [
+            devPath + "**/*.model.js",
+            devPath + "**/*.module.js",
+            devPath + "**/*.core.js",
+            devPath + "**/app.js",
+            devPath + "**/app.run.js",
+            devPath + "**/*.config.js",
+            devPath + "**/*.services.js",
+            devPath + "**/*.routes.js",
+            devPath + "**/*.directive.js",
+            devPath + "**/*.widget.js",
+            devPath + "**/*.filter.js",
+            devPath + "**/*.controller.js",
+            devPath + "**/*.js",
+            "!" + devPath + "**/*.spec.js"
+        ],
+
+        layoutPage: layout,
+        layoutInjector: layoutInjector
+
+    };
+
+    config.getWiredepDefaultOptions = function () {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+        return options;
+    };
+
+    return config;
+};
