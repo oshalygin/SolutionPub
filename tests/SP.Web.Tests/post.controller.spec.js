a5c6e3c963a20115499a95d7a17373ec8c955ad2
oshalygin <oshalygin@gmail.com>
2015-10-08 21:35:01 -0700

    tests are borked there is a dotnet5 to dotnet46 reference issue

--- tests/SP.Web.Tests/post.controller.spec.js
+++ tests/SP.Web.Tests/post.controller.spec.js
@@ -1,15 +1,34 @@
-/// <reference path="../../src/SP/wwwwroot/_references.js" />
+/// <reference path="../../src/sp/wwwroot/app.js" />
+
 
 
 (function () {
     describe("PostController:", function () {
 
-        // beforeEach(module("app"));
-        // beforeEach(module("app.post"));
+        var $controller;
+        var scope = {};
+        var $rootScope;
+
+        beforeEach(module("app"));
+        //  beforeEach(module("app.post"));
+
+        beforeEach(inject(function (_$controller_) {
+            $controller = _$controller_;
+        }));
+
+
+
+
 
         it("Sanity Check", function () {
             expect(true).toBeTruthy();
         });
+
+        it("page title is Solution Pub", function () {
+            var controller = $controller('PasswordController', { $scope: $scope });
+            expect($scope.title).is.equal.to("Solution Pub");
+        });
+
 
     });
 })();
