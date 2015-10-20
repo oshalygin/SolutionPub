aea4f3754189aaa595d6a0de6e26c44b30f7b46b
oshalygin <oshalygin@gmail.com>
2015-10-19 19:24:14 -0700

    prepping failed xunit test to see if travisCI picks it up

---
+++ tests/SP.Web.Tests/XUnitTestsAreActive.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SP.WEB.Tests
+{
+    public class XUnitTestsAreActive
+    {
+
+        [Fact]
+        public void ConfigurationIsSuccessful()
+        {
+            var expected = 5 + 5;
+            var actual = 8;
+
+            Assert.Equal(expected,actual);
+        }
+    }
+}
