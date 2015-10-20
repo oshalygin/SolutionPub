39ebc8cbab2260d69c284ee0496b8f8420778849
oshalygin <oshalygin@gmail.com>
2015-10-19 19:43:18 -0700

    removed period from dnx run command and added ControllerTests class

---
+++ tests/SP.Web.Tests/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using SP.WEB.Controllers;
+using Xunit;
+
+namespace SP.WEB.Tests
+{
+    public class HomeControllerTests
+    {
+
+        [Fact]
+        public void IndexReturnsView()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index() as ViewResult;
+            Assert.NotNull(result);
+
+
+        }
+
+    }
+}
