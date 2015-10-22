2f3064eb367f8f9d5ba3ffef31cfa6d1c3666a7e
oshalygin <oshalygin@gmail.com>
2015-10-21 20:02:28 -0700

    somehow tagbll got missed

--- src/SP.BLL/TagBLL.cs
+++ src/SP.BLL/TagBLL.cs
@@ -1,6 +1,38 @@
-﻿namespace SP.BLL
+﻿using System.Collections.Generic;
+using SP.Entities;
+
+namespace SP.BLL
 {
     public class TagBLL: ITagBLL
     {
+        public Tag GetTagById(int tagId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Tag> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Tag GetMostCommonTag()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Tag EditTag(Tag tag)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool DeleteTag(int tagId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Tag SaveTag(Tag tag)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
