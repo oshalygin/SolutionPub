81f1264478d08ea9d981ee8a86a6f6db25bddd35
oshalygin <oshalygin@gmail.com>
2015-10-22 22:14:55 -0700

    setting up imageutility to process images and resize them

---
+++ src/SP.Utilities/IImageUtility.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using ImageProcessor;
+
+namespace SP.Utilities
+{
+
+    //TODO: Truly consider this 3rd party library, seems cumbersome
+    public interface IImageUtility
+    {
+        bool ValidFileExtension(string extension);
+        ImageFactory ResizeImage(int imageWidth, int imageHeight, Stream filePath);
+        string GetServerPath(string originalPhotoUrlPath);
+        bool SaveImage(ImageFactory image);
+        bool FormatImage(ImageFactory image);
+
+    }
+}
---
+++ src/SP.Utilities/ImageUtility.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using ImageProcessor;
+
+namespace SP.Utilities
+{
+    public class ImageUtility: IImageUtility
+    {
+        public bool ValidFileExtension(string extension)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ImageFactory ResizeImage(int imageWidth, int imageHeight, Stream filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetServerPath(string originalPhotoUrlPath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool SaveImage(ImageFactory image)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool FormatImage(ImageFactory image)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
