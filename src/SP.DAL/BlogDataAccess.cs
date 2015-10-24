182a7aa75dc4884f899ba02a6811e16345415b3e
oshalygin <oshalygin@gmail.com>
2015-10-24 12:03:46 -0700

    working on DAL

--- src/SP.DAL/BlogDataAccess.cs
+++ src/SP.DAL/BlogDataAccess.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Data.Entity;
 using SP.Entities;
 
 namespace SP.DAL
@@ -15,18 +17,55 @@ namespace SP.DAL
 
         public IEnumerable<Post> GetPosts(int page, int pageSize)
         {
-            throw new System.NotImplementedException();
+            return _context
+                .Posts
+                .Include(x => x.Tags)
+                .Include(x => x.Comments)
+                .OrderBy(x => x.PostedDate)
+                .Skip(page*pageSize)
+                .Take(pageSize);
         }
 
         public IEnumerable<Tag> GetTopTags(int maxNumberOfTags)
         {
-            throw new System.NotImplementedException();
+            return _context
+                .Tags
+                .OrderBy(x => x.TimesUsed)
+                .Take(maxNumberOfTags);
         }
 
         public IEnumerable<Post> GetLatestPosts(int quantity)
         {
-            throw new System.NotImplementedException();
+            return _context
+                .Posts
+                .OrderBy(x => x.PostedDate)
+                .Take(quantity);
         }
+
+        public IEnumerable<Tag> GetTags()
+        {
+            return _context
+                .Tags
+                .OrderBy(x => x.TimesUsed);
+
+        }
+
+        public Tag EditTag(Tag tag)
+        {
+            var retrievedTag = _context.Tags.FirstOrDefault(x => x.Id == tag.Id);
+
+            if (retrievedTag == null)
+            {
+                return null;
+            }
+
+            retrievedTag.TimesUsed = tag.TimesUsed;
+            retrievedTag.Name = tag.Name;
+            _context.SaveChanges();
+            return tag;
+        }
+
+
     }
 
 }
--- src/SP.Utilities/IImageUtility.cs
+++ src/SP.Utilities/IImageUtility.cs
@@ -4,7 +4,6 @@
 namespace SP.Utilities
 {
 
-    //TODO: Truly consider this 3rd party library, seems cumbersome
     public interface IImageUtility
     {
         bool ValidFileExtension(string extension);
