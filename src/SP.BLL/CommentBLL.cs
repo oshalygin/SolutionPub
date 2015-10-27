11ebec29a78493cbdd2a9198510eef86d1a8389e
Oleg Shalygin <oshalygin@gmail.com>
2015-10-26 22:29:38 -0700

    merging files to work on new comp, will resolve this before going live

--- src/SP.BLL/CommentBLL.cs
+++ src/SP.BLL/CommentBLL.cs
@@ -1,6 +1,12 @@
-﻿namespace SP.BLL
+﻿using SP.Entities;
+
+namespace SP.BLL
 {
     public class CommentBLL: ICommentBLL
     {
+        public Comment GetCommentById(int commentId)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
