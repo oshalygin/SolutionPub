d69fb6d19c5cf694d030deab302cfe9689bdff5e
Oleg Shalygin <oshalygin@gmail.com>
2015-11-20 20:57:46 -0800

    front end model updates for angular

--- src/SP.WEB/application/models/tweets.model.ts
+++ src/SP.WEB/application/models/tweets.model.ts
@@ -3,14 +3,24 @@ module app.models {
     export interface ITweet {
 
         id: number;
-        postedDated: Date;
+        originalPostedDate: Date;
+        weeksFromPostedDate: number;
+        daysFromPostedDate: number;
+        hoursFromPostedDate: number;
+        minutesFromPostedDate: number;
+        secondsFromPostedDate: number;
         body: string;
     }
 
     export class Tweet implements ITweet {
 
         id: number;
-        postedDated: Date;
+        originalPostedDate: Date;
+        weeksFromPostedDate: number;
+        daysFromPostedDate: number;
+        hoursFromPostedDate: number;
+        minutesFromPostedDate: number;
+        secondsFromPostedDate: number;
         body: string;
     }
 }
