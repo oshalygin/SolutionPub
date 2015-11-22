10591423596efc84bee730ed556f0a840f699c84
Oleg Shalygin <oshalygin@gmail.com>
2015-11-21 23:37:02 -0800

    refactored twitter configuration to pull from config.json

--- src/SP.Twitter/TwitterApi.cs
+++ src/SP.Twitter/TwitterApi.cs
@@ -1,4 +1,9 @@
-﻿namespace SP.Twitter
+﻿using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace SP.Twitter
 {
     public class TwitterApi : ITwitterApi
     {
@@ -8,18 +13,44 @@
         //TODO: Hardcoded for now, need to pull from config.json
         public TwitterApi()
         {
-            _timelineSettings = new TimelineSettings();
-            _timelineSettings.UserName = "oshalygin";
-            _timelineSettings.Count = 3;
-            _timelineSettings.ExcludeReplies = "0";
-            _timelineSettings.IncludeRetweets = "1";
-            _timelineSettings.TimelineFormat =
-                @"https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={0}&amp;include_rts={1}&amp;exclude_replies={2}&amp;count={3}";
+            const string baseTwitterConfiguration = "Data:TwitterConfiguration";
+            string username;
+            string tweetCount;
+            string excludeReplies;
+            string includeRetweets;
+            string timelineFormat;
+            string oAuthUrl;
+            string oAuthConsumerKey;
+            string oAuthConsumerSecret;
+            
+            var configuration = new ConfigurationBuilder().SetBasePath(@"..\")
+                .AddJsonFile("config.json");
 
-            _authenticationSettings = new AuthenticationSettings();
-            _authenticationSettings.OauthUrl = @"https://api.twitter.com/oauth2/token";
-            _authenticationSettings.OauthConsumerKey = "QaM8auViHNQCp1JhB9Mpo6u1F";
-            _authenticationSettings.OauthConsumerSecret = "15NYd2AKxkL9Vix3xBRUPsKhb0Znxodg019Wv2jCzL3qQBZZ2l";
+            var twitterConfiguration = configuration.Providers.Single();
+            _timelineSettings = new TimelineSettings();
+            
+            twitterConfiguration.TryGet($"{baseTwitterConfiguration}:username", out username);
+            twitterConfiguration.TryGet($"{baseTwitterConfiguration}:count", out tweetCount);
+            twitterConfiguration.TryGet($"{baseTwitterConfiguration}:excludeReplies", out excludeReplies);
+            twitterConfiguration.TryGet($"{baseTwitterConfiguration}:includeRetweets", out includeRetweets);
+            twitterConfiguration.TryGet($"{baseTwitterConfiguration}:timelineFormat", out timelineFormat);
+            twitterConfiguration.TryGet($"{baseTwitterConfiguration}:oAuthUrl", out oAuthUrl);
+            twitterConfiguration.TryGet($"{baseTwitterConfiguration}:oAuthConsumerKey", out oAuthConsumerKey);
+            twitterConfiguration.TryGet($"{baseTwitterConfiguration}:oAuthConsumerSecret", out oAuthConsumerSecret);
+
+            _timelineSettings.UserName = username;
+            _timelineSettings.Count = int.Parse(tweetCount);
+            _timelineSettings.ExcludeReplies = excludeReplies;
+            _timelineSettings.IncludeRetweets = includeRetweets;
+            _timelineSettings.TimelineFormat = timelineFormat;
+
+            _authenticationSettings = new AuthenticationSettings
+            {
+                OauthUrl = oAuthUrl,
+                OauthConsumerKey = oAuthConsumerKey,
+                OauthConsumerSecret = oAuthConsumerSecret
+            };
+            
         }
 
         public string GetTimeline()
