f40b8666c5ff4e5d83684628390073dbe7d163d1
Oleg Shalygin <oshalygin@gmail.com>
2015-12-09 20:08:55 -0800

    added extension method to get distinctby

---
+++ src/SP.Utilities/LinqExtensionHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SP.Utilities
+{
+    public static class LinqExtensionHelpers
+    {
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            var availableKeys = new HashSet<TKey>();
+            return source
+                .Where(element => availableKeys.Add(keySelector(element)));
+        }
+    }
+}
